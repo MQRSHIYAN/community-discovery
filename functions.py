6bcd960648544e7ceacd6a169e9734908da87f78
Fran Lozano <franloza@ucm.es>
2017-01-18 15:13:46 +0100

    Added support functions for ranking calculation and subgraph extraction from communities

---
+++ functions.py
@@ -0,0 +1,80 @@
+import community
+import networkx as nx
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+
+#Functions for loading/saving objects
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+#Community functions
+''' Return list of tuples with the communities and its number of members
+    sorted by number of members'''
+def ranking_members_community (partition,from_below=False):
+    countCommunities = []
+    for com in set(partition.values()):
+        count = sum( x == com for x in partition.values())
+        countCommunities.append([com,count])
+    ranking = sorted(countCommunities, key=lambda tup: tup[1],reverse= not from_below)
+    return ranking
+
+''' Return induced graph of top N communities with more members'''
+def induced_graph_ranking(G,partition,ranking,topN):
+    [subG,reduced_partition] = subgraph_ranking((G,partition,ranking,topN))
+    ind = community.induced_graph(reduced_partition, subG)
+    return ind
+
+''' Return sub graph and reduced partition of top N communities with more members'''
+def subgraph_ranking(G,partition,ranking,topN):
+    communities = [x[0] for x in ranking[0:topN]]
+    reduced_partition =  { k:v for k, v in partition.items() if v in communities}
+    nodes = reduced_partition.keys()
+    subG = G.subgraph(nodes)
+    return [subG,reduced_partition]
+
+
+''' Return sub graph of a certain community'''
+def subgraph_community(G,partition,community):
+    nodes = list((k) for k,v in partition.items() if v == community)
+    subG = G.subgraph(nodes)
+    return subG
+
+##Role functions
+''' Return list of tuples with the top N opinion leaders and their score'''
+def opinion_leaders (G,topN):
+    scores = nx.pagerank(G)
+    ranking = sorted(scores, key=scores.get,reverse=True)
+    if (topN > len(scores)): topN = len(scores)
+    topRanking=[]
+    for i in range(0,topN):
+        topRanking.append([ranking[i],scores[str(ranking[i])]])
+    return topRanking
+
+#Display functions
+def display_partition (G,partition,figureNumber=1,edgeWidth=1,nodeSize=10):
+    
+    size = float(len(set(partition.values())))
+
+    #Define colors of the clusters
+    norm = colors.Normalize(vmin=0, vmax=size, clip=True)
+    mapper = cm.ScalarMappable(norm=norm, cmap=cm.rainbow)
+
+    #Display partition
+    plt.figure(figureNumber)
+    pos = nx.random_layout(G)
+    count = 0
+    for com in set(partition.values()) :
+        count = count + 1.
+        list_nodes = [nodes for nodes in partition.keys()
+                                    if partition[nodes] == com]
+        nx.draw_networkx_nodes(G, pos, list_nodes, node_size = nodeSize,
+                                    node_color = mapper.to_rgba(count))
+    nx.draw_networkx_edges(G,pos, alpha=0.2, width=edgeWidth)
+    plt.show()
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cm
-import pickle
+import functions as fx
 
 ############## CONFIGURATION ##############
-dataset = 1
+dataset = 2
 load_partition=True
 display_plots=False
 ###########################################
@@ -43,6 +43,7 @@ if display_plots:
 if load_partition:
     #Load partition from file
     partition = load_obj('partition_dataset_' + str(dataset))
+    size = float(len(set(partition.values())))
 else:
     #Compute the best partition
     partition = community.best_partition(G)
@@ -56,24 +57,7 @@ else:
 print('Modularity: {0:.4f}'.format(community.modularity(partition,G)))
 
 if display_plots:
-    #Define colors of the clusters
-    norm = colors.Normalize(vmin=0, vmax=size, clip=True)
-    mapper = cm.ScalarMappable(norm=norm, cmap=cm.rainbow)
-
-    #Display partition
-    plt.figure(2)
-    pos = nx.random_layout(G)
-    count = 0
-    for com in set(partition.values()) :
-        count = count + 1.
-        list_nodes = [nodes for nodes in partition.keys()
-                                    if partition[nodes] == com]
-        nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 10,
-                                    node_color = mapper.to_rgba(count))
-
-
-    nx.draw_networkx_edges(G,pos, alpha=0.5)
-    plt.show()
+    display_partition(G,partition,2)
 
 #Calculate induced graph
 #ind = community.induced_graph(partition, G)
@@ -81,31 +65,12 @@ if display_plots:
 #Display induced graph
 #if display_plots:
 #   plt.figure(3)
-#   pos = nx.spring_layout(ind)
+#   pos = nx.random_layout(ind)
 #   nx.draw_networkx_edges(ind,pos, alpha=0.5)
 #   nx.draw_networkx_nodes(ind, pos,node_size=40,node_color='1')
 #   plt.show()
 
 #Get Opinion Leaders(OP)
-opinion_leaders(20)
+print(opinion_leaders(G,20))
 
-#Function for saving objects
-def save_obj(obj, name ):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def load_obj(name):
-    with open('obj/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
-
-##Role functions
-
-''' Return list of tuples with the top N opinion leaders and their score'''
-def opinion_leaders (G,topN):
-    scores = nx.pagerank(G)
-    ranking = sorted(scores, key=scores.get,reverse=True)
-    if (topN > len(scores)): topN = len(scores)
-    topRanking=[]
-    for i in range(0,topN):
-        topRanking.append([ranking[i],scores[str(ranking[i])]])
-    return topRanking
